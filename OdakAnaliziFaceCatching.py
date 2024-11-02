3182f3893ac106c6eb6378635423b7be12f8d9b9
Fase2507 <113676981+Fase2507@users.noreply.github.com>
2024-11-02 19:40:42 +0300

    Update OdakAnaliziFaceCatching.py

--- OdakAnaliziFaceCatching.py
+++ OdakAnaliziFaceCatching.py
@@ -1,70 +1,64 @@
-import cv2
+
 import numpy as np
+import time
+import cv2
 
-
-
-###STACK###
-def stackImages(scale,imgArray):
+# Stack function
+def stackImages(scale, imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
     width = imgArray[0][0].shape[1]
     height = imgArray[0][0].shape[0]
     if rowsAvailable:
-        for x in range ( 0, rows):
-            for y in range(0, cols):
-                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+        for x in range(rows):
+            for y in range(cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
                     imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
                 else:
                     imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-        imageBlank = np.zeros((height, width, 3), np.uint8)
-        hor = [imageBlank]*rows
-        hor_con = [imageBlank]*rows
-        for x in range(0, rows):
-            hor[x] = np.hstack(imgArray[x])
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        hor = [np.hstack(imgArray[x]) for x in range(rows)]
         ver = np.vstack(hor)
     else:
-        for x in range(0, rows):
-            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-            else:
-                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-        hor= np.hstack(imgArray)
+        hor = np.hstack([cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR) if len(imgArray[x].shape) == 2 else imgArray[x] for x in range(rows)])
         ver = hor
     return ver
 
-###########
 def empty(a):
     pass
-#CASCADES
 
+# Cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_lefteye_2splits.xml')
-faceCascade= cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+global imgEye, imgEyeHSV
 
+# Video Capture
 cap = cv2.VideoCapture(0)
-cap.set(3,360)
-cap.set(4,480)
-cap.set(12,250)
+cap.set(3, 360)
+cap.set(4, 480)
+cap.set(12, 250)
 
-####TrackBar#####
+# Trackbars for HSV values
 cv2.namedWindow("Trackbars")
-cv2.resizeWindow("Trackbars",640,240)
+cv2.resizeWindow("Trackbars", 640, 240)
 
-cv2.createTrackbar("Hue min","Trackbars",0,179,empty)
-cv2.createTrackbar("Hue max","Trackbars",179,179,empty)
-cv2.createTrackbar("Sat min","Trackbars",0,255,empty)
-cv2.createTrackbar("Sat max","Trackbars",255,255,empty)
-cv2.createTrackbar("Val min","Trackbars",36,255,empty)
-cv2.createTrackbar("Val max","Trackbars",255,255,empty)
-###
+cv2.createTrackbar("Hue min", "Trackbars", 0, 179, empty)
+cv2.createTrackbar("Hue max", "Trackbars", 179, 179, empty)
+cv2.createTrackbar("Sat min", "Trackbars", 0, 255, empty)
+cv2.createTrackbar("Sat max", "Trackbars", 255, 255, empty)
+cv2.createTrackbar("Val min", "Trackbars", 36, 255, empty)
+cv2.createTrackbar("Val max", "Trackbars", 255, 255, empty)
+
+focus_loss_start_time = None
+total_elapsed_time = 0  # Toplam süre
 
 while True:
     success, img = cap.read()
-    ###HUE-SAT-VAL####
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
+    # Trackbar values
     h_min = cv2.getTrackbarPos("Hue min", "Trackbars")
     h_max = cv2.getTrackbarPos("Hue max", "Trackbars")
     s_min = cv2.getTrackbarPos("Sat min", "Trackbars")
@@ -72,184 +66,42 @@ while True:
     v_min = cv2.getTrackbarPos("Val min", "Trackbars")
     v_max = cv2.getTrackbarPos("Val max", "Trackbars")
 
-    # print(h_min,h_max,s_min,v_min,v_max)
-    # color=(255,0,0)
     lower = np.array([h_min, s_min, v_min])
     upper = np.array([h_max, s_max, v_max])
     mask = cv2.inRange(imgHSV, lower, upper)
-
-    # origin=(100,100)
     imgResult = cv2.bitwise_and(img, img, mask=mask)
-    ##################
-    faces = faceCascade.detectMultiScale(img, 1.2, 4)
+
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-     ##eye
     Faces = face_cascade.detectMultiScale(gray, 1.2, 4)
-    imgCropped = img[50:450, 80:250]
-    global imgEye, imgEyeHSV
-
     for (x, y, w, h) in Faces:
+        imgEye = img[y:y + h, x:x + w]
+        imgEyeHSV = cv2.inRange(imgEye, lower, upper)
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 1)
-        imgEye = img[y:y + h,x:x + h]
-        imgEyeHSV = cv2.inRange(imgEye,lower,upper)
-    global imgHead
-    #cv2.imshow("Result", img)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        imgHead = img[y:y + h, x:x + w]
 
+    white_area = cv2.countNonZero(imgEyeHSV)
+    total_area = imgEyeHSV.shape[0] * imgEyeHSV.shape[1]
+    white_ratio = white_area / total_area
 
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        break
-    imgStack = stackImages(0.8, ([img, imgResult, mask],
-                                                [imgEye,imgEyeHSV,imgHead] ))
+    # Eğer beyaz oranı %95'ten fazlaysa süreyi başlat
+    if white_ratio > 0.95:
+        if focus_loss_start_time is None:
+            focus_loss_start_time = time.time()
+        else:
+            elapsed_time = time.time() - focus_loss_start_time
+            if elapsed_time >= 5:  # 5 saniye geçtiğinde toplam süreyi güncelle
+                total_elapsed_time += elapsed_time
+                focus_loss_start_time = time.time()  # Başlangıç zamanını güncelle
+    else:
+        focus_loss_start_time = None  # Beyaz alan oranı %95'in altına düştüğünde başlangıç zamanını sıfırla
 
-    # cv2.imshow("original", img)
-    # cv2.imshow("result", imgResult)
-    # cv2.imshow("Msk", mask)
+    cv2.putText(img, f"Odak kaybi: {int(total_elapsed_time)} sn", (10, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    imgStack = stackImages(0.8, [[img, imgResult, mask], [imgEye, imgEyeHSV, img]])
     cv2.imshow("ALL", imgStack)
 
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 cap.release()
-if v_min==36:
-    print("ODAKLI")###17 179 0 255 31 255 önemli deger.
-else:
-    print("HATA")
-# if h_max < 100:
-#     cv2.putText(img, "hata", (200, 130), cv2.FONT_HERSHEY_SIMPLEX, 1, color)
-print(h_min,h_max,s_min,s_max,v_min ,v_max)
-
-#0 61 0 75 93
-#0 179 0 94 145!! onemli eşik degeri.
-#0 179 0 75 145!! Negatif eşik odak yok!!
-
-cv2.waitKey(0)
-##ESKISI ALTTA
-
-
-
-
-
-
-
-
-
-
-
-##ESKISI ALTTA
-# import cv2
-# import numpy as np
-# ###STACK###
-# def stackImages(scale,imgArray):
-#     rows = len(imgArray)
-#     cols = len(imgArray[0])
-#     rowsAvailable = isinstance(imgArray[0], list)
-#     width = imgArray[0][0].shape[1]
-#     height = imgArray[0][0].shape[0]
-#     if rowsAvailable:
-#         for x in range ( 0, rows):
-#             for y in range(0, cols):
-#                 if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
-#                     imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
-#                 else:
-#                     imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
-#                 if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
-#         imageBlank = np.zeros((height, width, 3), np.uint8)
-#         hor = [imageBlank]*rows
-#         hor_con = [imageBlank]*rows
-#         for x in range(0, rows):
-#             hor[x] = np.hstack(imgArray[x])
-#         ver = np.vstack(hor)
-#     else:
-#         for x in range(0, rows):
-#             if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
-#                 imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
-#             else:
-#                 imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
-#             if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
-#         hor= np.hstack(imgArray)
-#         ver = hor
-#     return ver
-#
-# ###########
-# def empty(a):
-#     pass
-# #CASCADES
-#
-# face_cascade = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
-# faceCascade= cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-#
-# cap = cv2.VideoCapture(0)
-# cap.set(3,360)
-# cap.set(4,480)
-# cap.set(12,250)
-#
-# ####TrackBar#####
-# cv2.namedWindow("Trackbars")
-# cv2.resizeWindow("Trackbars",640,240)
-#
-# cv2.createTrackbar("Hue min","Trackbars",0,179,empty)
-# cv2.createTrackbar("Hue max","Trackbars",179,179,empty)
-# cv2.createTrackbar("Sat min","Trackbars",0,255,empty)
-# cv2.createTrackbar("Sat max","Trackbars",255,255,empty)
-# cv2.createTrackbar("Val min","Trackbars",0,255,empty)
-# cv2.createTrackbar("Val max","Trackbars",255,255,empty)
-# ###
-#
-# while True:
-#     success, img = cap.read()
-#     ###HUE-SAT-VAL####
-#     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-#
-#     h_min = cv2.getTrackbarPos("Hue min", "Trackbars")
-#     h_max = cv2.getTrackbarPos("Hue max", "Trackbars")
-#     s_min = cv2.getTrackbarPos("Sat min", "Trackbars")
-#     s_max = cv2.getTrackbarPos("Sat max", "Trackbars")
-#     v_max = cv2.getTrackbarPos("Val max", "Trackbars")
-#     v_min = cv2.getTrackbarPos("Val min", "Trackbars")
-#     # print(h_min,h_max,s_min,v_min,v_max)
-#     color=(255,0,0)
-#     lower = np.array([h_min, s_min, v_min])
-#     upper = np.array([h_max, s_max, v_max])
-#     mask = cv2.inRange(imgHSV, lower, upper)
-#     origin=(100,100)
-#     imgResult = cv2.bitwise_and(img, img, mask=mask)
-#     ##################
-#     faces = faceCascade.detectMultiScale(img, 1.2, 4)
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#     Faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-#     imgCropped = img[50:350, 50:150]
-#
-#     for (x, y, w, h) in Faces:
-#         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-#     #cv2.imshow("Result", img)
-#     for (x, y, w, h) in faces:
-#         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-#
-#
-#     if cv2.waitKey(1) & 0xFF ==ord('q'):
-#         break
-#     imgStack = stackImages(0.8, ([img, imgResult, mask,imgCropped]))
-#
-#     # cv2.imshow("original", img)
-#     # cv2.imshow("result", imgResult)
-#     # cv2.imshow("Msk", mask)
-#     cv2.imshow("ALL", imgStack)
-#
-# cap.release()
-# if (h_min ==0 and  h_max ==179) and (s_min==0 and s_max==255) and (v_min>=74 and v_max<93):
-#     print("ODAKLI")
-# else:
-#     print("HATA")
-# # if h_max < 100:
-# #     cv2.putText(img, "hata", (200, 130), cv2.FONT_HERSHEY_SIMPLEX, 1, color)
-# print(h_min,h_max,s_min,s_max,v_min ,v_max)
-#
-# #0 61 0 75 93
-# #0 179 0 94 145!! onemli eşik degeri.
-# #0 179 0 75 145!! Negatif eşik odak yok!!
-#
-# cv2.waitKey(0)
-#
-#
-#
-#
+cv2.destroyAllWindows()
